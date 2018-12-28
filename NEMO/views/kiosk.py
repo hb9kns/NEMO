e20e96c0921f0afacc557ebf373aea964dcbc43a
dsbarth <34581202+dsbarth@users.noreply.github.com>
2018-12-28 11:18:34 -0500

    Fixed bug in kiosk caused by previous changes to tool summary

--- NEMO/views/kiosk.py
+++ NEMO/views/kiosk.py
@@ -111,7 +111,7 @@ def choices(request):
 		'customer': customer,
 		'usage_events': UsageEvent.objects.filter(operator=customer.id, end=None).order_by('tool__name').prefetch_related('tool', 'project'),
 		'tools': Tool.objects.filter(visible=True, location=request.GET['location']),
-		'tool_summary': create_tool_summary(),
+		'tool_summary': create_tool_summary(request),
 		'categories': [t[0] for t in Tool.objects.filter(visible=True).order_by('category').values_list('category').distinct()],
 	}
 	return render(request, 'kiosk/choices.html', dictionary)
@@ -131,7 +131,7 @@ def category_choices(request, category, user_id):
 		'customer': customer,
 		'category': category,
 		'tools': tools,
-		'tool_summary': create_tool_summary(),
+		'tool_summary': create_tool_summary(request),
 	}
 	return render(request, 'kiosk/category_choices.html', dictionary)
 
