3d06b09d561968aa6c585e5fb58c37133ecf040a
dsbarth <34581202+dsbarth@users.noreply.github.com>
2018-12-28 11:15:47 -0500

    Fixed some outstanding issues from the merge

--- start_NEMO_in_Docker.sh
+++ start_NEMO_in_Docker.sh
@@ -4,10 +4,11 @@
 set -e
 
 # Run migrations to create or update the database
+django-admin makemigrations NEMO
 django-admin migrate
 
 # Collect static files
 django-admin collectstatic --no-input --clear
 cp -a /usr/local/lib/python3.6/site-packages/NEMO/migrations/. /nemo/migrations/copy
 # Run NEMO
-gunicorn --config=/etc/gunicorn_configuration.py NEMO.wsgi:application
+gunicorn --bind 0.0.0.0:8000 NEMO.wsgi:application
