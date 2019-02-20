bc9d512f3a23170c34719b0c025804e4632a4e9e
David S Barth <dbarth@princeton.edu>
2019-02-20 14:56:50 -0500

    Bugfix in db authentication

--- NEMO/views/db_authentication.py
+++ NEMO/views/db_authentication.py
@@ -1,30 +1,32 @@
 # /home/nemo/python/lib/python3.6/site-packages/NEMO/views/
 # settings.py: AUTHENTICATION_BACKENDS = ['NEMO.views.db_authentication.DbAuthenticationBackend']
 
-from django.contrib.auth import get_user_model
 from django.contrib.auth.hashers import check_password
 from django.contrib.auth.backends import ModelBackend
 from django.utils.decorators import method_decorator
 from django.views.decorators.debug import sensitive_post_parameters
-from NEMO.models import UserAuth
+from NEMO.models import User, UserAuth
 
 class DbAuthenticationBackend(ModelBackend):
 
-    @method_decorator(sensitive_post_parameters('password'))
-    def authenticate(self, request, username=None, password=None, **keyword_arguments):
-        User = get_user_model()
-        try:
-            user_auth = UserAuth.objects.get(username=username)
-            if user_auth.validate_password(password):
-                user = User.objects.get(username=username)
-                return user
-        except:
-            return None
-        return None
+	@method_decorator(sensitive_post_parameters('password'))
+	def authenticate(self, request, username=None, password=None, **keyword_arguments):
+		if not username or not password:
+			return None
+		try:
+			user = User.objects.get(username=username)
+			user_auth = UserAuth.objects.get(username=username)
+		except User.DoesNotExist:
+			#logger.warning(f"Username {username} attempted to authenticate with LDAP, but that username does not exist in the NEMO database. The user was denied access.")
+			return None
 
-    def get_user(self, user_id):
-        User = get_user_model()
-        try:
-            return User.objects.get(pk=user_id)
-        except User.DoesNotExist:
-            return None
+		if not user.is_active:
+			#logger.warning(f"User {username} successfully authenticated with LDAP, but that user is marked inactive in the NEMO database. The user was denied access.")
+			return None
+
+		try:
+			if check_password(password, user_auth.hash):
+				return user
+		except:
+			return None
+		return None
