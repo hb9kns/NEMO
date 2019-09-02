8474d92387d19f4d9318106c3436065bd0d44e55
Yargo B <yargo+test@phys.ethz.ch>
2019-09-02 15:02:46 +0200

    simplify startup, use django directly without gunicorn

--- runwsgi.sh
+++ runwsgi.sh
@@ -2,10 +2,9 @@
 # install NEMO from working directory into following venv directory:
 NEMODIR=/var/local/nemo
 NEMODB=nemo.db
-# gunicorn listener
-GLISTN=127.0.1.1:12345
-# used for testing whether installation already took place
-DONE=$NEMODIR/static
+# listener parameters
+LISTN=127.0.1.1:12345
+LISTN=localhost:8080
 
 if test "$NEMODIR" = ""
 then cat <<EOH
@@ -21,39 +20,40 @@ python -m venv $NEMODIR
 export DJANGO_SETTINGS_MODULE=settings
 export PYTHONPATH=$NEMODIR/
 
-if test -d $DONE
-then echo :: already installed, skipping
-else
-
-
- echo ::::: make sure everything is running on python3.6 at minimum :::::
- sleep 1
- echo :: installing python packages
- pip install wheel
+echo ::::: make sure everything is running on python3.6 at minimum :::::
+sleep 1
+echo :: installing python packages
+pip install wheel
 #  # following may not be necessary if gunicorn is installed on site:
 #  pip install gunicorn
- pip install NEMO
+pip install NEMO
 
- cp -f ./*.py $NEMODIR
- cp -f /etc/ssl/certs/quovadis_global_ica_g2.crt $NEMODIR/quovadis.crt
+mkdir -p $NEMODIR/logs $NEMODIR/secrets
+cp -f ./*.py $NEMODIR
+cp -f /etc/ssl/certs/quovadis_global_ica_g2.crt $NEMODIR/secrets/quovadis.crt
+touch $NEMODIR/logs/django_error.log
+
+if test -r $NEMODIR/$NEMODB
+then echo :: $NEMODIR/$NEMODB already installed, skipping
+else
  if test -r $NEMODB
  then cp -f $NEMODB $NEMODIR/$NEMODB
  fi
-
- mkdir -p $NEMODIR/logs
- touch $NEMODIR/logs/django_error.log
- cd $NEMODIR
- ln -s lib/python3.*/site-packages/NEMO/migrations migrations
-
- echo :: preparing django
-#  Run migrations to create or update the database
- django-admin makemigrations NEMO
- django-admin migrate
-#  Collect static files
- django-admin collectstatic --no-input --clear
-
 fi
 
-echo :: running gunicorn test server on $GLISTN
-cd $NEMODIR/lib/python3.*/site-packages/
-gunicorn --bind $GLISTN NEMO.wsgi:application
+cd $NEMODIR
+ln -s lib/python3.*/site-packages/NEMO/migrations migrations
+
+echo :: preparing django
+#  Run migrations to create or update the database
+django-admin makemigrations NEMO
+django-admin migrate
+#  Collect static files
+django-admin collectstatic --no-input --clear
+
+export REMOTE_USER=yargo
+
+echo :: running test server on $LISTN
+# cd $NEMODIR/lib/python3.*/site-packages/
+# gunicorn --bind $LISTN NEMO.wsgi:application
+django-admin runserver $LISTN
