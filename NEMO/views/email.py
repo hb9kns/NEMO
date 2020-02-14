8da6e3ad0f056857fc35ccedf42ced5bb9d0d6fa
Yargo Bonetti DPHYS <yargo@phys.ethz.ch>
2020-02-14 12:11:31 +0100

    (forgot email view in last commit)

--- NEMO/views/email.py
+++ NEMO/views/email.py
@@ -80,7 +80,7 @@ def email_broadcast(request, audience=''):
 		dictionary['search_base'] = Project.objects.filter(active=True, account__active=True)
 	elif audience == 'account':
 		dictionary['search_base'] = Account.objects.filter(active=True)
-	elif audience == 'all' or audience == 'equiresp' or audience == 'mentors' or audience == 'pjtresp' :
+	elif audience == 'all' or audience == 'equiresp' or audience == 'pjtresp' :
 		dictionary['search_base'] = 'all'
 		dictionary['all'] = True
 	dictionary['audience'] = audience
@@ -101,8 +101,6 @@ def compose_email(request):
 				users = User.objects.filter(projects__account__id=selection).distinct()
 			elif audience == 'equiresp':
 				users = User.objects.filter(groups__name='Equipment Responsibles')
-			elif audience == 'mentors':
-				users = User.objects.filter(groups__name='Mentors')
 			elif audience == 'all':
 				users = User.objects.all()
 			else:
@@ -160,8 +158,6 @@ def send_broadcast_email(request):
 			users = User.objects.filter(projects__account__id=selection)
 		elif audience == 'equiresp':
 			users = User.objects.filter(groups__name='Equipment Responsibles')
-		elif audience == 'mentors':
-			users = User.objects.filter(groups__name='Mentors')
 		elif audience == 'all':
 			users = User.objects.all()
 		if active_choice:
