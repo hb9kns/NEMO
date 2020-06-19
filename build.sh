0c93a6ca39ffeddd6f448cbb8fa02d8b1bd3381f
Yargo Bonetti DPHYS <yargo@phys.ethz.ch>
2020-06-19 18:01:03 +0200

    cleanup bumped from first-prod

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# copy localized files
-for nn in Dockerfile settings.py startNEMO.sh
-do /bin/cp -f ../our-nemo/$nn .
-done
-# build docker image
-docker build --tag nemof -f Dockerfile .
