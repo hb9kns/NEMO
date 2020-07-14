8f86101a1f182528a0e1e9501b9765d927419be1
Yargo Bonetti DPHYS <yargo@phys.ethz.ch>
2020-07-14 12:30:42 +0200

    begin usage statistics view

--- NEMO/urls.py
+++ NEMO/urls.py
@@ -9,6 +9,7 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.views.generic.base import RedirectView
 
 from NEMO.views import abuse, accounts_and_projects, alerts, api, area_access, authentication, beacon, calendar, configuration_agenda, consumables, customization, directory, email, feedback, get_projects, history, jumbotron, kiosk, landing, maintenance, mobile, usage, news, qualifications, remote_work, resources, safety, sensors, sidebar, staff_charges, status_dashboard, stockroom, tasks, tool_control, training, tutorials, users, user_chemicals, forgot_password, billing, consultation
+#from NEMO.views import toolstats
 
 # Use our custom login page instead of Django's built-in one.
 admin.site.login = login_required(admin.site.login)
@@ -262,6 +263,9 @@ if settings.ALLOW_CONDITIONAL_URLS:
 		#billing
 		url(r'^billing/$', billing.billing, name='billing'),
 		url(r'^billingxls/$', billing.billingxls, name='billingxls'),
+
+		# tool usage statistics
+#		url(r'^toolstats/$', toolstats.tool, name='toolstats'),
 
 		# General area occupancy table, for use with Kiosk and Area Access tablets
 		url(r'^occupancy/$', status_dashboard.occupancy, name='occupancy'),
