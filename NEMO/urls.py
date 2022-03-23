906603995fea5de12c9cfb533d172b3af56f5da2
Yargo Bonetti DPHYS <yargo@phys.ethz.ch>
2022-03-23 15:25:48 +0100

    add admin link to all toolresponsibles, hide invisible tools

--- NEMO/urls.py
+++ NEMO/urls.py
@@ -208,6 +208,7 @@ urlpatterns = [
 	url(r'^directory/$', directory.directory, name='directory'),
 	url(r'^userlist/$', directory.userlist, name='userlist'),
 	url(r'^toolresponsibles/(?P<namesuffix>[0-9A-Za-z_-]*)$', directory.toolresponsibles, name='toolresponsibles'),
+	url(r'^toolresponsibles/$', directory.toolresponsibles, name='allresponsibles'),
 
 	# Area access:
 	url(r'^change_project/$', area_access.change_project, name='change_project'),
