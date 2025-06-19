03daaa34d8ec091ebedf5cc262e1b5e5463d563e
Yargo Bonetti DPHYS <yargo@phys.ethz.ch>
2025-06-19 08:41:57 +0200

    update authentication

--- NEMO/views/authentication.py
+++ NEMO/views/authentication.py
@@ -11,142 +11,165 @@ from django.urls import reverse, resolve
 from django.utils.decorators import method_decorator
 from django.views.decorators.debug import sensitive_post_parameters
 from django.views.decorators.http import require_http_methods, require_GET
-from ldap3 import Tls, Server, Connection, AUTO_BIND_TLS_BEFORE_BIND, SIMPLE
+from ldap3 import Tls, Server, Connection, SIMPLE, NONE
 from ldap3.core.exceptions import LDAPBindError, LDAPExceptionError
 
 from NEMO.models import User
 from NEMO.views.customization import get_media_file_contents
 
-
-logger = getLogger('django.request')
+logger = getLogger(__name__)
 
 class RemoteUserAuthenticationBackend(RemoteUserBackend):
-	""" The web server performs Kerberos authentication and passes the user name in via the REMOTE_USER environment variable. """
-	create_unknown_user = False
+    """ The web server performs Kerberos authentication and passes the user name in via the REMOTE_USER environment variable. """
+    create_unknown_user = False
 
-	def clean_username(self, username):
-		"""
-		User names arrive in the form user@DOMAIN.NAME.
-		This function chops off Kerberos realm information (i.e. the '@' and everything after).
-		"""
-		return username.partition('@')[0]
+    def clean_username(self, username):
+        """
+        User names arrive in the form user@DOMAIN.NAME.
+        This function chops off Kerberos realm information (i.e. the '@' and everything after).
+        """
+        return username.partition('@')[0]
 
 
 class NginxKerberosAuthorizationHeaderAuthenticationBackend(ModelBackend):
-	""" The web server performs Kerberos authentication and passes the user name in via the HTTP_AUTHORIZATION header. """
+    """ The web server performs Kerberos authentication and passes the user name in via the HTTP_AUTHORIZATION header. """
 
-	def authenticate(self, request, username=None, password=None, **keyword_arguments):
-		# Perform any custom security checks below.
-		# Returning None blocks the user's access.
-		username = self.clean_username(request.META.get('HTTP_AUTHORIZATION', None))
+    def authenticate(self, request, username=None, password=None, **keyword_arguments):
+        # Perform any custom security checks below.
+        # Returning None blocks the user's access.
+        username = self.clean_username(request.META.get('HTTP_AUTHORIZATION', None))
 
-		# The user must exist in the database
-		try:
-			user = User.objects.get(username=username)
-		except User.DoesNotExist:
-			logger.warning(f"Username {username} attempted to authenticate with Kerberos via Nginx, but that username does not exist in the NEMO database. The user was denied access.")
-			return None
+        # The user must exist in the database
+        try:
+            user = User.objects.get(username=username)
+        except User.DoesNotExist:
+            logger.warning(f"Username {username} attempted to authenticate with Kerberos via Nginx, but that username does not exist in the NEMO database. The user was denied access.")
+            return None
 
-		# The user must be marked active.
-		if not user.is_active:
-			logger.warning(f"User {username} successfully authenticated with Kerberos via Nginx, but that user is marked inactive in the NEMO database. The user was denied access.")
-			return None
+        # The user must be marked active.
+        if not user.is_active:
+            logger.warning(f"User {username} successfully authenticated with Kerberos via Nginx, but that user is marked inactive in the NEMO database. The user was denied access.")
+            return None
 
-		# All security checks passed so let the user in.
-		logger.debug(f"User {username} successfully authenticated with Kerberos via Nginx and was granted access to NEMO.")
-		return user
+        # All security checks passed so let the user in.
+        logger.debug(f"User {username} successfully authenticated with Kerberos via Nginx and was granted access to NEMO.")
+        return user
 
-	def clean_username(self, username):
-		"""
-		User names arrive encoded in base 64, similar to Basic authentication, but with a bogus password set (since .
-		This function chops off Kerberos realm information (i.e. the '@' and everything after).
-		"""
-		if not username:
-			return None
-		pieces = username.split()
-		if len(pieces) != 2:
-			return None
-		if pieces[0] != "Basic":
-			return None
-		return b64decode(pieces[1]).decode().partition(':')[0]
+    def clean_username(self, username):
+        """
+        User names arrive encoded in base 64, similar to Basic authentication, but with a bogus password set (since .
+        This function chops off Kerberos realm information (i.e. the '@' and everything after).
+        """
+        if not username:
+            return None
+        pieces = username.split()
+        if len(pieces) != 2:
+            return None
+        if pieces[0] != "Basic":
+            return None
+        return b64decode(pieces[1]).decode().partition(':')[0]
 
 
 class LDAPAuthenticationBackend(ModelBackend):
-	""" This class provides LDAP authentication against an LDAP or Active Directory server. """
+    """ This class provides LDAP authentication against an LDAP or Active Directory server. """
 
-	@method_decorator(sensitive_post_parameters('password'))
-	def authenticate(self, request, username=None, password=None, **keyword_arguments):
-		if not username or not password:
-			return None
+    @method_decorator(sensitive_post_parameters('password'))
+    def authenticate(self, request, username=None, password=None, **keyword_arguments):
+        if not username or not password:
+            return None
 
-		# The user must exist in the database
-		try:
-			user = User.objects.get(username=username)
-		except User.DoesNotExist:
-			logger.warning(f"Username {username} attempted to authenticate with LDAP, but that username does not exist in the NEMO database. The user was denied access.")
-			return None
+        # The user must exist in the database
+        try:
+            user = User.objects.get(username=username)
+        except User.DoesNotExist:
+            logger.warning(f"Username {username} attempted to authenticate with LDAP, but that username does not exist in the NEMO database. The user was denied access.")
+            return None
 
-		# The user must be marked active.
-		if not user.is_active:
-			logger.warning(f"User {username} successfully authenticated with LDAP, but that user is marked inactive in the NEMO database. The user was denied access.")
-			return None
+        # The user must be marked active.
+        if not user.is_active:
+            logger.warning(f"User {username} successfully authenticated with LDAP, but that user is marked inactive in the NEMO database. The user was denied access.")
+            return None
 
-		for server in settings.LDAP_SERVERS:
-			try:
-				t = Tls(validate=CERT_REQUIRED, version=PROTOCOL_TLSv1_2, ca_certs_file=server['certificate'])
-				s = Server(server['url'], port=636, use_ssl=True, tls=t)
-				c = Connection(s, user='{}={},{}'.format(server['attribute'], username, server['base']), password=password, auto_bind=AUTO_BIND_TLS_BEFORE_BIND, authentication=SIMPLE)
-				c.unbind()
-				# At this point the user successfully authenticated to at least one LDAP server.
-				return user
-			except LDAPBindError as e:
-				logger.warning(f"User {username} attempted to authenticate with LDAP, but entered an incorrect password. The user was denied access.")
-				pass  # When this error is caught it means the username and password were invalid against the LDAP server.
-			except LDAPExceptionError as e:
-				exception(e)
+        for server in settings.LDAP_SERVERS:
+            try:
+                tls = Tls(
+                    validate=CERT_REQUIRED,
+                    version=PROTOCOL_TLSv1_2,
+                    ca_certs_file=server['certificate']
+                )
+                s = Server(
+                    server['url'],
+                    use_ssl=True,
+                    tls=tls,
+                    get_info=NONE
+                )
+                c = Connection(
+                    s,
+                    authentication="SIMPLE",
+                    user='{}={},{}'.format(server['attribute'],username, server['base']),
+                    password=password,
+                    auto_bind="NONE",
+                    version=3,
+                    client_strategy="SYNC"
+                )
+                c.open()
+                c.start_tls()
 
-		# The user did not successfully authenticate to any of the LDAP servers.
-		return None
+                if c.bind():
+                    c.unbind()
+                    # At this point the user successfully authenticated to at least one LDAP server.
+                    return user
+                else:
+                    logger.warning(f"User {username} attempted to authenticate with LDAP, but entered an incorrect password. The user was denied access.")
+
+            except LDAPBindError as e:
+                logger.warning(f"User {username} attempted to authenticate with LDAP, but entered an incorrect password. The user was denied access.")
+                pass  # When this error is caught it means the username and password were invalid against the LDAP server.
+            except LDAPExceptionError as e:
+                exception(e)
+
+        # The user did not successfully authenticate to any of the LDAP servers.
+        return None
 
 
 @require_http_methods(['GET', 'POST'])
 @sensitive_post_parameters('password')
 def login_user(request):
-	if 'NEMO.views.authentication.RemoteUserAuthenticationBackend' in settings.AUTHENTICATION_BACKENDS or 'NEMO.views.authentication.NginxKerberosAuthorizationHeaderAuthenticationBackend' in settings.AUTHENTICATION_BACKENDS:
-		if request.user.is_authenticated:
-			return HttpResponseRedirect(reverse('landing'))
-		else:
-			return render(request, 'authorization_failed.html')
+    if 'NEMO.views.authentication.RemoteUserAuthenticationBackend' in settings.AUTHENTICATION_BACKENDS or 'NEMO.views.authentication.NginxKerberosAuthorizationHeaderAuthenticationBackend' in settings.AUTHENTICATION_BACKENDS:
+        if request.user.is_authenticated:
+            return HttpResponseRedirect(reverse('landing'))
+        else:
+            return render(request, 'authorization_failed.html')
 
-	dictionary = {
-		'login_banner': get_media_file_contents('login_banner.html'),
-		'user_name_or_password_incorrect': False,
-		'version': settings.VERSIONID,
-		'remote_host': request.META.get('REMOTE_ADDR', '(unknown)'),
-	}
-	if 'NEMO.views.db_authentication.DbAuthenticationBackend' in settings.AUTHENTICATION_BACKENDS:
-		dictionary['reset_password_link'] = reverse('forgot_password')
-	else:
-		dictionary['reset_password_link'] = ""
+    dictionary = {
+        'login_banner': get_media_file_contents('login_banner.html'),
+        'user_name_or_password_incorrect': False,
+        'version': settings.VERSIONID,
+        'remote_host': request.META.get('REMOTE_ADDR', '(unknown)'),
+    }
+    if 'NEMO.views.db_authentication.DbAuthenticationBackend' in settings.AUTHENTICATION_BACKENDS:
+        dictionary['reset_password_link'] = reverse('forgot_password')
+    else:
+        dictionary['reset_password_link'] = ""
 
-	if request.method == 'GET':
-		return render(request, 'login.html', dictionary)
-	username = request.POST.get('username', '')
-	password = request.POST.get('password', '')
-	user = authenticate(request, username=username, password=password)
-	if user:
-		login(request, user)
-		try:
-			next_page = request.GET[REDIRECT_FIELD_NAME]
-			resolve(next_page)  # Make sure the next page is a legitimate URL for NEMO
-		except:
-			next_page = reverse('landing')
-		return HttpResponseRedirect(next_page)
-	dictionary['user_name_or_password_incorrect'] = True
-	return render(request, 'login.html', dictionary)
+    if request.method == 'GET':
+        return render(request, 'login.html', dictionary)
+    username = request.POST.get('username', '')
+    password = request.POST.get('password', '')
+    user = authenticate(request, username=username, password=password)
+    if user:
+        login(request, user)
+        try:
+            next_page = request.GET[REDIRECT_FIELD_NAME]
+            resolve(next_page)  # Make sure the next page is a legitimate URL for NEMO
+        except:
+            next_page = reverse('landing')
+        return HttpResponseRedirect(next_page)
+    dictionary['user_name_or_password_incorrect'] = True
+    return render(request, 'login.html', dictionary)
 
 
 @require_GET
 def logout_user(request):
-	logout(request)
-	return HttpResponseRedirect(reverse('landing'))
+    logout(request)
+    return HttpResponseRedirect(reverse('landing'))
